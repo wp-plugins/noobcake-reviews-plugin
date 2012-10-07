d7d9e37e411b2c144d1facd9f708ab0313f81762
jimburnettva <jimburnettva@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-07 15:03:20 +0000

    3.4.2 changes
    git-svn-id: https://plugins.svn.wordpress.org/noobcake-reviews-plugin/trunk@609184 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- noobcakereviewsplugin.php
+++ noobcakereviewsplugin.php
@@ -3,7 +3,7 @@
 Plugin Name: NoobcakeReviewsPlugin
 Plugin URI: http://www.jimmyburnett.com
 Description: A super basic reviews plugin which shows a 1 to 5 star rating. Simply add a custom_field value called "score" between 1 to 5 to a blog post and stars will appears where you place the code. 
-Version: 1.0
+Version: 1.1
 Author: Jimmy Burnett 
 Author URI: http://www.jimmyburnett.com
 License: GPL2
